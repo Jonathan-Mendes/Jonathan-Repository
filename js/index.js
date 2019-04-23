42a17f27a32e16c64ea508a66b6ccf430a069a4f
Jonathan Mendes <jonathan_mendes96@outlook.com>
2019-04-23 14:11:48 -0300

    Game Jogo da Velha 2.0

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-var jogador =1
+var jogador = 1
+var g1 = 0
+var g2 = 0
+var g3 = 0
+var g4 = 0
+var g5 = 0
+var g6 = 0
+var g7 = 0
+var g8 = 0
 
-initialize()
 start()
-
-function initialize(){
-	// jogador = 1
-	start()
-}
 
 function start(){
 	play(jogador)
 }
 
 function play(player){
-
 	var imgA1 = document.getElementById('a1')
 	var imgA2 = document.getElementById('a2')
 	var imgA3 = document.getElementById('a3')
@@ -25,42 +26,73 @@ function play(player){
 	var imgC3 = document.getElementById('c3')
 	
 	imgA1.onclick = function(){
-		imgA1.src = endereco()
-		trocaPlayer() 
+		if(imgA1){
+			trocaImg(imgA1)
+			imgA1 = false
+			validar('a1')
+		}
 	}
 	imgA2.onclick = function(){
-		imgA2.src = endereco()
-		trocaPlayer() 
+		if(imgA2){
+			trocaImg(imgA2)
+			imgA2 = false
+			validar('a2')
+		}
 	}
 	imgA3.onclick = function(){
-		imgA3.src = endereco()
-		trocaPlayer() 
+		if(imgA3){
+			trocaImg(imgA3)
+			imgA3 = false
+			validar('a3')
+		}
 	}
 	imgB1.onclick = function(){
-		imgB1.src = endereco()
-		trocaPlayer() 
+		if(imgB1){
+			trocaImg(imgB1)
+			imgB1 = false
+			validar('b1')
+		}
 	}
 	imgB2.onclick = function(){
-		imgB2.src = endereco()
-		trocaPlayer() 
+		if(imgB2){
+			trocaImg(imgB2)
+			imgB2 = false
+			validar('b2')
+		}
 	}
 	imgB3.onclick = function(){
-		imgB3.src = endereco()
-		trocaPlayer() 
+		if(imgB3){
+			trocaImg(imgB3)
+			imgB3 = false
+			validar('b3')
+		}
 	}
 	imgC1.onclick = function(){
-		imgC1.src = endereco()
-		trocaPlayer() 
+		if(imgC1){
+			trocaImg(imgC1)
+			imgC1 = false
+			validar('c1')
+		}
 	}
 	imgC2.onclick = function(){
-		imgC2.src = endereco()
-		trocaPlayer() 
+		if(imgC2){
+			trocaImg(imgC2)
+			imgC2 = false
+			validar('c2')
+		}
 	}
 	imgC3.onclick = function(){
-		imgC3.src = endereco()
-		trocaPlayer() 
+		if(imgC3){
+			trocaImg(imgC3)
+			imgC3 = false
+			validar('c3')
+		}
 	}
+}
 
+function trocaImg(img){
+	img.src = endereco()
+	trocaPlayer()
 }
 
 function trocaPlayer(){
@@ -69,7 +101,6 @@ function trocaPlayer(){
 	}else if(jogador === 2){
 		jogador = 1
 	}
-	console.log(jogador)
 }
 
 function endereco(end){
@@ -80,4 +111,77 @@ function endereco(end){
 		console.log("jogador2")
 		return "img/o.jpg"
 	}
+}
+
+function validar(v){
+	
+	if(v === 'a1'){
+		g1++
+		g4++
+		g7++
+		if(g1 === 3 || g4 === 3 || g7 === 3){
+			console.log("entrou")
+			playerWin()
+		}
+	}else if(v === 'a2'){
+		g1++
+		g5++
+		if(g1 === 3 || g5 ===  3){
+			playerWin()
+		}
+	}else if(v === 'a3'){
+		g1++
+		g6++
+		g8++
+		
+		if(g1 === 3 || g6 ===  3 || g8 === 3){
+			playerWin()
+		}
+	}else if(v === 'b1'){
+		g2++
+		g4++
+		if(g2 === 3 || g4 ===  3){
+			playerWin()
+		}
+	}else if(v === 'b2'){
+		g2++
+		g5++
+		g7++
+		g8++
+		if(g2 === 3 || g5 ===  3 || g7 === 3 || g8 === 3){
+			playerWin()
+		}
+	}else if(v === 'b3'){
+		g2++
+		g6++
+		if(g2 === 3 || g6 ===  3){
+			playerWin()
+		}
+	}
+	else if(v === 'c1'){
+		g3++
+		g4++
+		g8++
+		if(g3 === 3 || g4 ===  3 || g8 === 3){
+			playerWin()
+		}
+	}
+	else if(v === 'c2'){
+		g3++
+		g5++
+		if(g3 === 3 || g5 ===  3){
+			playerWin()
+		}
+	}
+	else if(v === 'c3'){
+		g3++
+		g6++
+		g7++
+		if(g3 === 3 || g6 ===  3 || g7 === 3){
+			playerWin()
+		}
+	}
+}
+function playerWin(){
+	console.log("ganhou")
 }
